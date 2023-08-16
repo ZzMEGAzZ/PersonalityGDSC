93aaa77e84b6a2b5bf99b6828221b9faa943e2ee
ZzMEGAzZ <apisitsangkrajang@gmail.com>
2023-08-17 02:40:16 +0700

    Update Header.svelte

--- src/component/Header.svelte
+++ src/component/Header.svelte
@@ -3,7 +3,7 @@
 	import LanguageToggle from './LanguageToggle.svelte';
 </script>
 
-<div class="z-50 inset-0 p-10 w-[90%] h-20 flex flex-row justify-between items-center">
+<div class="z-50 inset-0 p-5 tablet:p-10 w-[95%] tablet:w-[90%] h-20 flex flex-row justify-between items-center">
 	<div class="flex flex-row items-center space-x-5">
 		<div class="w-20 h-20">
 			<GdscSvg />
